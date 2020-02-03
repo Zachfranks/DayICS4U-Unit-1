f0146178fbdc9c2fe45e16bd1ed322f3d16fda34
Zachfranks <43444948+Zachfranks@users.noreply.github.com>
2020-02-03 18:13:11 -0500

    Added Day#2 part 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String firstWord = "Hello";
+        String secondWord = " World";
+        System.out.println(firstWord + secondWord);
+        //creates two words "Hello" and "World" then adds them in the console
+
+        String nameContex = "Name:";
+        String firstName = " Zachary";
+        String secondName = " Franks";
+        System.out.println(nameContex + firstName + secondName );
+        //displays a word "Name" and shows to put a name next to then displays the name "Zachary" then adds the last name "Franks" in the console
+
+        String ageContex = "Age:";
+        String ageNumber = " 17";
+        System.out.println(ageContex + ageNumber );
+        //displays a word "Age" and shows to put the age next to then displays the number 17 in the console
+        ;
+
+    }
+}
